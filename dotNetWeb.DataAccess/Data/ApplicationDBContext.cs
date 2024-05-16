175b479c0892d025e417daa59ce2f0af6ab2e36b
ASUS\mahbu <hasanmahbub331@gmail.com>
2024-05-17 00:33:08 +0600

    Basic DB Integration is done, now CRUD can be implemented

---
+++ dotNetWeb.DataAccess/Data/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using dotNetWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetWeb.DataAccess.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category {Id = 1, Name = "Action", DisplayOrder=1},
+                new Category {Id = 2, Name = "Scify", DisplayOrder=2},
+                new Category {Id = 3, Name = "History", DisplayOrder=3}
+                );
+        }
+
+    }
+}
---
+++ dotNetWeb/Controllers/CategoryController.cs
@@ -0,0 +1,87 @@
+﻿using dotNetWeb.DataAccess.Data;
+using dotNetWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetWeb.Controllers
+{
+
+    
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult CreateNewCategory()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult CreateNewCategory(Category obj)
+        {
+            if(ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                TempData["success"] = "New category added successfully";
+                return RedirectToAction("Index");
+            }
+            return View();  
+        }
+
+        
+        public IActionResult EditEntry(int? id) {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+           
+            if (category == null) {
+                return NotFound();
+            }
+            
+
+
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult EditEntry(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+
+
+        }
+
+        public IActionResult Delete(int ob)
+        {
+            Category category = _db.Categories.Find(ob);
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+
+                TempData["success"] = "Category deleted successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
